db796ead4acc68913194155cbdbb0c2f4e05654d
George Dilthey <george.dilthey@gmail.com>
2021-03-09 20:13:03 -0500

    added print method to recipe

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -10,5 +10,31 @@ class Recipe
         @steps = steps
     end
 
+    def print_recipe
+        puts "Here's your recipe:"
+        puts " "
+        puts "Recipe: #{self.title}"
+        puts "Description: #{self.description}"
+        display_ingredients(ingredients)
+        display_steps(steps)
+        
+            
+    end
+    
+    def display_ingredients(array)
+        puts "Ingredients:"
+        array.map{|i| puts " • #{i}"}
+    end
+
+    def display_steps(array)
+        puts "Steps:"
+        step_count = 1
+        array.each do |i|
+            puts "  #{step_count}. #{i}"
+            step_count += 1
+        end
+    end
+
+
 end
 
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,21 +12,20 @@ class Scraper
 
     def get_recipe_by_schema(url)
         schema = get_schema(url)
-        schema.each do |i| 
-            type = i["@type"]
-            if type == "Recipe"
-                title = i["name"]
-                description = i["description"]
-                ingredients = i["recipeIngredient"]
-                steps = i["recipeInstructions"].map {|ri| ri["text"]}
-                create_new_recipe(title,description,ingredients,steps)
-            end
-        end  
+
+        title = schema["name"]
+        description = schema["description"]
+        ingredients = schema["recipeIngredient"]
+        steps = schema["recipeInstructions"].map {|instruction| instruction["text"].gsub("\n","")}
+           
+        create_new_recipe(title,description,ingredients,steps)
+ 
     end
 
     def get_schema(url)   
         noko = Nokogiri::HTML(open(url))
-        schema = JSON.parse(noko.css('script[type*="application/ld+json"]').text)
+        schemas = JSON.parse(noko.css('script[type*="application/ld+json"]').text)
+        recipe_schema = schemas.find{|i| i["@type"] == "Recipe"}
     end
 
     def create_new_recipe(title, description, ingredients, steps)
@@ -35,6 +34,8 @@ class Scraper
 
 end
 
-Scraper.new.get_recipe_by_schema('https://www.allrecipes.com/recipe/10813/best-chocolate-chip-cookies/')
+recipe = Scraper.new.get_recipe_by_schema('https://www.foodnetwork.com/recipes/ina-garten/blueberry-coffee-cake-muffins-recipe-1917173')
+
+recipe.print_recipe
 
 
