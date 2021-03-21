139c2fea9a4baae42ba0253d1841d300347d4a48
George Dilthey <george.dilthey@gmail.com>
2021-03-21 00:10:01 -0400

    Updated scraper to support more schema layouts

--- lib/just-the-recipe/scraper.rb
+++ lib/just-the-recipe/scraper.rb
@@ -16,16 +16,29 @@ class JustTheRecipe::Scraper
         title = schema["name"]
         description = schema["description"]
         ingredients = schema["recipeIngredient"]
-        steps = schema["recipeInstructions"].map {|instruction| instruction["text"].gsub("\n","")}
+        if schema["recipeInstructions"][0]["itemListElement"]
+            steps = schema["recipeInstructions"].map {|section| section["itemListElement"].map {|instruction| instruction["text"].gsub("\n","")}}.flatten
+        else
+            steps = schema["recipeInstructions"].map {|instruction| instruction["text"].gsub("\n","")}
+        end
         create_new_recipe(title,description,ingredients,steps, @url)
     end
 
     def get_schema   
         noko = Nokogiri::HTML(open(@url))
         js = (noko.css('script[type*="application/ld+json"]'))
-        js.length == 1 ? schemas = JSON.parse(js.text) : schemas = js.map {|i| valid_json?(i.text) ? JSON.parse(i.text) : nil }   
-        recipe_schema = schemas.find{|i| i["@type"] == "Recipe"}
-        
+        if js.length == 1 
+            parsed = JSON.parse(js.text)
+            if parsed.class == Hash
+                graph = parsed["@graph"]
+                recipe = graph.find{|i| i["@type"] == "Recipe"}
+            else
+                recipe = parsed.find{|i| i["@type"] == "Recipe"}
+            end
+        else
+            parsed = js.map {|i| valid_json?(i.text) ? JSON.parse(i.text) : nil }
+            recipe = parsed.find{|i| i["@type"] == "Recipe"}
+        end
     end
 
     def create_new_recipe(title, description, ingredients, steps, url)
@@ -50,8 +63,9 @@ class JustTheRecipe::Scraper
 
 end
 
-# all_recipe = JustTheRecipe::Scraper.new('http://www.seriouseats.com/recipes/2011/03/bread-baking-70-percent-hydration-bread-recipe.html').get_recipe_by_schema.display_recipe
-# # serious = JustTheRecipe::Scraper.new('https://www.seriouseats.com/recipes/2021/03/orecchiette-con-le-cime-di-rapa.html').get_recipe_by_schema
+# veg = JustTheRecipe::Scraper.new('https://www.vegrecipesofindia.com/eggless-chocolate-chip-muffins-recipe/').get_recipe_by_schema.display_recipe
+# all_recipe = JustTheRecipe::Scraper.new('https://www.allrecipes.com/recipe/10813/best-chocolate-chip-cookies/').get_recipe_by_schema.display_recipe
+# serious = JustTheRecipe::Scraper.new('https://www.seriouseats.com/recipes/2021/03/orecchiette-con-le-cime-di-rapa.html').get_recipe_by_schema.display_recipe
 
 # all_recipe.display_recipe
 
